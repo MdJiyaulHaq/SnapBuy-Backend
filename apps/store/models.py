383dc95467416df1164cde9b64e9ea69f20725e1
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2026-01-16 16:51:43 +0545

    fix: allow blank values for featured_product in Collection model

--- apps/store/models.py
+++ apps/store/models.py
@@ -20,7 +20,7 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+        "Product", on_delete=models.SET_NULL, null=True, blank=True, related_name="+"
     )
 
     def __str__(self):
